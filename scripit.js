5bed10ee6fa07f88c716c1b3712254b3a81b2e20
fmsDesign <brandon300@ymail.com>
2024-07-04 11:17:44 -0300

    melhorias na area de faq

---
+++ scripit.js
@@ -0,0 +1,9 @@
+const animaisIMG = document.querySelectorAll("img");
+
+const linksInternos = document.querySelectorAll('[href^="#"]');
+
+const h2Selecao = document.querySelector(".animais-conteudo h2");
+
+const paragrafos = document.querySelectorAll("p");
+
+const lastParagrafo = paragrafos[--paragrafos.length];
