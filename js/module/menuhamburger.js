1eab8cb9b91ad6268f07dec2123113866a54ea86
bikcraft <brandon300@ymail.com>
2024-07-20 13:29:51 -0300

    erro menu hamburger

--- js/module/menuhamburger.js
+++ js/module/menuhamburger.js
@@ -3,24 +3,22 @@ import outsideClick from "./outsideClick.js";
 export default function initMenuHamburger() {
   const btnHamburger = document.querySelector("[data-menu='button']");
   const listaHamburger = document.querySelector("[data-menu='lista']");
-  const dataDropdown = document.querySelector("[data-dropdown]");
   const eventos = ["click", "touchstart"];
 
   if (btnHamburger) {
     function expandMenu(item) {
       const btnFalse = btnHamburger.getAttribute("aria-expanded");
+      listaHamburger.classList.add("ativo");
+      // if (btnFalse === "false") {
+      //   this.setAttribute("aria-expanded", "true");
+      //   listaHamburger.classList.add("ativo");
+      // } else {
+      //   this.setAttribute("aria-expanded", "false");
+      //   listaHamburger.classList.add("ativo");
+      // }
 
-      if (btnFalse === "false") {
-        this.setAttribute("aria-expanded", "true");
-        listaHamburger.classList.add("ativo");
-      } else {
-        this.setAttribute("aria-expanded", "false");
-        listaHamburger.classList.add("ativo");
-      }
-      setTimeout(() =>
-        outsideClick(listaHamburger, eventos, () =>
-          listaHamburger.classList.remove("ativo")
-        )
+      outsideClick(listaHamburger, eventos, () =>
+        listaHamburger.classList.remove("ativo")
       );
     }
     eventos.forEach((evento) =>
