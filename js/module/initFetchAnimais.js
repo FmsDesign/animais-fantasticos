4cf0ffdc3296b054b71b05da7af7cc18c84962f0
fmsDesign <brandon300@ymail.com>
2024-08-01 13:40:27 -0300

    scrollsuave transformado em classe

--- js/module/initFetchAnimais.js
+++ js/module/initFetchAnimais.js
@@ -1,8 +1,31 @@
-export default function initFetchAnimais() {}
-
 // import initNum from "./numeros.js";
 
-// async function fetchAnimais(url) {
+// export default function initFetchAnimais() {
+//   async function fetchAnimais(url) {
+//     const animaisResponse = await fetch(url);
+//     const animaisJson = await animaisResponse.json();
+//     const container = document.querySelector(".numeros");
+
+//     animaisJson.forEach((animal) => {
+//       const divAnimal = createAnimal(animal);
+//       container.appendChild(divAnimal);
+//       initNum();
+//     });
+//   }
+
+//   function createAnimal(animal) {
+//     const div = document.createElement("div");
+//     div.innerHTML = ` <h3 class="font-sub-display cor-9">${animal.specie}</h3>
+//         <span data-animaNum class="font-m2 cor-s2">${animal.total}</span>`;
+//     return div;
+//   }
+
+//   fetchAnimais("../animaisapi.json");
+// }
+
+// // import initNum from "./numeros.js";
+
+// // async function fetchAnimais(url) {
 //   const animaisResponse = await fetch(url);
 //   const animaisJson = await animaisResponse.json();
 
@@ -18,26 +41,3 @@ export default function initFetchAnimais() {}
 //   initNum();
 // }
 // fetchAnimais("../js/animaisapi.json");
-
-import initNum from "./numeros.js";
-
-async function fetchAnimais(url) {
-  const animaisResponse = await fetch(url);
-  const animaisJson = await animaisResponse.json();
-  const container = document.querySelector(".numeros");
-
-  animaisJson.forEach((animal) => {
-    const divAnimal = createAnimal(animal);
-    container.appendChild(divAnimal);
-    initNum();
-  });
-}
-
-function createAnimal(animal) {
-  const div = document.createElement("div");
-  div.innerHTML = ` <h3 class="font-sub-display cor-9">${animal.specie}</h3>
-        <span data-animaNum class="font-m2 cor-s2">${animal.total}</span>`;
-  return div;
-}
-
-fetchAnimais("../../js/animaisapi.json");
--- js/module/scrollsuave.js
+++ js/module/scrollsuave.js
@@ -1,20 +1,34 @@
-export default function initScroll() {
-  const linksInternos = document.querySelectorAll(
-    "[data-anima='menu'] a[href^='#']"
-  );
-
-  if (linksInternos.length) {
-    function scrollSuave(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
-      section.scrollIntoView({
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = {
         behavior: "smooth",
         block: "start",
-      });
+      };
+    } else {
+      this.options = options;
     }
-    linksInternos.forEach((link) => {
-      link.addEventListener("click", scrollSuave);
+    this.scrollToSelection = this.scrollToSelection.bind(this);
+  }
+
+  scrollToSelection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSelection);
     });
+  }
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+
+    return this;
   }
 }
