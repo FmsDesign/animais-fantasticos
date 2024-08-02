1e88e9f1fd66b580c5ada3c64eedd620fac618ca
fmsDesign <brandon300@ymail.com>
2024-08-02 06:40:45 -0300

    accordinon transformado em classe

--- js/module/accordion.js
+++ js/module/accordion.js
@@ -1,19 +1,23 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    "[data-anima='accordion'] dt"
-  );
-  const ativo = "ativo";
+export default class Accordion {
+  constructor(lista) {
+    this.accordionList = document.querySelectorAll(lista);
+    this.activeClass = "ativo";
+  }
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
 
-  accordionList[0].classList.add(ativo);
-  accordionList[0].nextElementSibling.classList.add(ativo);
-
-  if (accordionList.length) {
-    function activeAccordion() {
-      this.classList.toggle(ativo);
-      this.nextElementSibling.classList.toggle(ativo);
-    }
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
+  }
+
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
